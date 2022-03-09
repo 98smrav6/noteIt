15b1538f59bf3ccd08ec361577cdba059995a22b
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2022-03-09 00:15:45 -0500

    function to add/remove note + npm i uuid

--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -29,5 +29,26 @@ class Store {
         });
     }
 
-    
+    addNote(note) {
+        const { title , text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+
+        const newNote = { title, text, id: uuidv1() };
+
+        return this,getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+
+    removeNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes));    
+    }
 }
+
+module.exports = new Store();
